0100f3f9f999d2cc80b51cbc03d9fc0ba406be15
Kyle Kendall <kkendall33@gmail.com>
2019-07-14 17:39:37 -0600

    Add coupon delete and create

--- CarSwaddleNetworkRequest/Services/CouponService.swift
+++ CarSwaddleNetworkRequest/Services/CouponService.swift
@@ -17,7 +17,11 @@ extension NetworkRequest.Request.Endpoint {
      /api/authorities
      /api/authorityRequests?pending=false
      */
-    fileprivate static let coupons = Request.Endpoint(rawValue: "/api/coupons")
+    private static let couponsAPI = "/api/coupons"
+    fileprivate static let coupons = Request.Endpoint(rawValue: Request.Endpoint.couponsAPI)
+    fileprivate static func coupons(with couponID: String) -> NetworkRequest.Request.Endpoint {
+        return Request.Endpoint(rawValue: Request.Endpoint.couponsAPI + "/" + couponID)
+    }
 }
 
 final public class CouponService: Service {
@@ -46,4 +50,70 @@ final public class CouponService: Service {
         }
     }
     
+    @discardableResult
+    public func deleteCoupon(couponID: String, completion: @escaping JSONCompletion) -> URLSessionDataTask? {
+        guard let urlRequest = serviceRequest.get(with: .coupons(with: couponID)) else { return nil }
+        return sendWithAuthentication(urlRequest: urlRequest) { data, error in
+            guard let data = data,
+                let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? JSONObject else {
+                    completion(nil, error)
+                    return
+            }
+            completion(json, error)
+        }
+    }
+    
+    @discardableResult
+    public func createCoupon(id: String, amountOff: Int?, percentOff: Int?, maxRedemptions: Int?, name: String, redeemBy: Date, discountBookingFee: Bool, isCorporate: Bool, completion: @escaping JSONCompletion) -> URLSessionDataTask? {
+        var jsonBody: [String:String] = [:]
+        jsonBody["id"] = id
+        if let amountOff = amountOff?.stringValue {
+            jsonBody["amountOff"] = amountOff
+        }
+        if let percentOff = percentOff?.stringValue {
+            jsonBody["percentOff"] = percentOff
+        }
+        if let maxRedemptions = maxRedemptions?.stringValue {
+            jsonBody["maxRedemptions"] = maxRedemptions
+        }
+        jsonBody["name"] = name
+        jsonBody["redeemBy"] = serverDateFormatter.string(from: redeemBy)
+        jsonBody["discountBookingFee"] = discountBookingFee.stringValue
+        jsonBody["isCorporate"] = isCorporate.stringValue
+        
+        guard let data = (try? JSONSerialization.data(withJSONObject: jsonBody, options: [])),
+            let urlRequest = serviceRequest.post(with: .coupons, body: data, contentType: .applicationJSON) else {
+                return nil
+        }
+        return sendWithAuthentication(urlRequest: urlRequest) { data, error in
+            guard let data = data,
+                let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? JSONObject else {
+                    completion(nil, error)
+                    return
+            }
+            completion(json, error)
+        }
+    }
+    
 }
+
+
+extension Int {
+    
+    public var stringValue: String {
+        return String(self)
+    }
+    
+}
+
+/*
+ {
+ "id": "testNoob",
+ "amountOff": 30,
+ "percentOff": null,
+ "maxRedemptions": 1,
+ "name": "Marks test 1",
+ "redeemBy": "2019-07-31T02:38:08.948Z",
+ "discountBookingFee": false
+ }
+ */
--- CarSwaddleNetworkRequestTests/CouponService.swift
+++ CarSwaddleNetworkRequestTests/CouponService.swift
@@ -16,10 +16,34 @@ class CouponTests: CarSwaddleLoginTestCase {
     
     let couponService = CouponService(serviceRequest: serviceRequest)
     
-    func testRequestAuthority() {
+    func testGetCoupons() {
         let exp = expectation(description: "\(#function)\(#line)")
         
         couponService.getCoupons(limit: 30, offset: 0) { json, error in
+            XCTAssert(json != nil && error == nil, "Should get values")
+            json?.printObject()
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 40, handler: nil)
+    }
+    
+    func testDeleteCoupon() {
+        let exp = expectation(description: "\(#function)\(#line)")
+        
+        couponService.deleteCoupon(couponID: "test") { json, error in
+            XCTAssert(json != nil && error == nil, "Should get values")
+            json?.printObject()
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 40, handler: nil)
+    }
+    
+    func testCreateCoupon() {
+        let exp = expectation(description: "\(#function)\(#line)")
+        
+        couponService.createCoupon(id: "test", amountOff: 30, percentOff: nil, maxRedemptions: 30, name: "Kyle's test coupon", redeemBy: Date().addingTimeInterval(24*60*60), discountBookingFee: false, isCorporate: true) { json, error in
             XCTAssert(json != nil && error == nil, "Should get values")
             json?.printObject()
             exp.fulfill()
