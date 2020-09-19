082dc969e8eead8764943f67f8ce9cf10ab5091c
Kyle Kendall <kkendall33@gmail.com>
2020-09-19 02:13:27 -0600

    Fix swift package

--- Package.swift
+++ Package.swift
@@ -14,13 +14,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "NetworkRequest", url: "https://github.com/Car-Swaddle/Authentication", .branch("master")),
+        .package(name: "Authentication", url: "https://github.com/Car-Swaddle/NetworkRequest", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CarSwaddleNetworkRequest",
-            dependencies: []),
+            dependencies: ["NetworkRequest", "Authentication"]),
         .testTarget(
             name: "CarSwaddleNetworkRequestTests",
             dependencies: ["CarSwaddleNetworkRequest"]),
